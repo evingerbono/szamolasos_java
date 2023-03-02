e4aaeb12a9881642e584df09d0d545fc00e7e0e5
evingerbono <bono0823@gmail.com>
2023-03-02 16:15:59 +0100

    felhasználótól_kérdés

--- src/ciklusok/Szamok.java
+++ src/ciklusok/Szamok.java
@@ -1,17 +1,24 @@
 package ciklusok;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Szamok {
 
     public static void main(String[] args) {
-
-        for (int a = 2; a <= 4; a++) {
-            for (int i = 2; i <= 4; i++) {
-                for (int x = 2; x <= 4; x++) {
-                    System.out.print(a + "" + i + "" + x + "\n");
+        Scanner sc = new Scanner(System.in, "latin2");
+        System.out.print("Hogyan jelenjelnek meg a számok (ismétlés/nemismétlés): ");
+        String valasz = sc.nextLine();
+        if (valasz == "ismétlés") {
+            for (int a = 2; a <= 4; a++) {
+                for (int i = 2; i <= 4; i++) {
+                    for (int x = 2; x <= 4; x++) {
+                        System.out.print(a + "" + i + "" + x + "\n");
+                    }
                 }
             }
+        }else{
+            
         }
 
     }
