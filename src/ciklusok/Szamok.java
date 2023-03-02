58f2086be19c0b38587056091bd8ccbb5b13057c
evingerbono <bono0823@gmail.com>
2023-03-02 16:03:44 +0100

    számok_kiíratása_egymásmellet_222,232...444

--- src/ciklusok/Szamok.java
+++ src/ciklusok/Szamok.java
@@ -5,11 +5,14 @@ import java.util.Random;
 public class Szamok {
 
     public static void main(String[] args) {
-        
-        for (int i = 2; i <=3; i++) {
-            for (int x = 2; x <= 4; x++) {
-                System.out.print(i+""+x+"\n");
+
+        for (int a = 2; a <= 4; a++) {
+            for (int i = 2; i <= 4; i++) {
+                for (int x = 2; x <= 4; x++) {
+                    System.out.print(a+""+i + "" + x + "\n");
+                }
             }
         }
+        
     }
 }
