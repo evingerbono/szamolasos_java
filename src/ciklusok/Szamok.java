58a561f9e56da4e3ae588066120e0b7883219e52
evingerbono <bono0823@gmail.com>
2023-03-02 16:00:19 +0100

    számok előállítása2,3,4

--- src/ciklusok/Szamok.java
+++ src/ciklusok/Szamok.java
@@ -1,8 +1,15 @@
-
 package ciklusok;
 
+import java.util.Random;
+
 public class Szamok {
+
     public static void main(String[] args) {
         
+        for (int i = 2; i <=3; i++) {
+            for (int x = 2; x <= 4; x++) {
+                System.out.print(i+""+x+"\n");
+            }
+        }
     }
 }
